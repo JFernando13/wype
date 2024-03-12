356c4bdd65f04a0be8567007bbe316b63f875d76
JFernando13 <juan_fer-13@hotmail.com>
2024-03-12 12:14:00 -0500

    current tab component v1

---
+++ app/components/tabs/current-tab.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import type { CurrentTabProps, TTab } from '@/components/tabs/types'
+import { ListItems } from './list-items'
+import { useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { useCompleted } from '@/store/useCompleted'
+
+export function CurrentTab() {
+  const searchParams = useSearchParams()
+  const { tabs } = useCompleted()
+
+  const tab = searchParams.get('category') ?? 'all'
+  const [currentTab, setCurrentTab] = useState<TTab>(tabs[0])
+
+  useEffect(
+    (effect) => {
+      setCurrentTab(tabs.find(({ name }) => name === tab) ?? tabs[1])
+    },
+    [tabs, tab]
+  )
+
+  return (
+    <section className='flex flex-col gap-6'>
+      {currentTab.content.map((content) => {
+        return (
+          <ListItems
+            key={content.name}
+            content={content}
+          />
+        )
+      })}
+    </section>
+  )
+}
