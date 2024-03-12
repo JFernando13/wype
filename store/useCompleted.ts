8246a9dc63f482884437236012e110a0e10130ba
JFernando13 <juan_fer-13@hotmail.com>
2024-03-12 12:15:37 -0500

    store v1

---
+++ store/useCompleted.ts
@@ -0,0 +1,45 @@
+import { create } from 'zustand'
+import { TTab, type TItem } from '@/app/components/tabs/types'
+import { tabs } from '@/app/components/tabs/mocks'
+
+type State = {
+  tabs: TTab[]
+}
+
+type Actions = {
+  addToCompleted: (item: TItem) => void
+}
+
+export const useCompleted = create<State & Actions>((set) => ({
+  tabs,
+  addToCompleted: (item) => {
+    // Encontrar la pestaña correspondiente
+
+    const currentTab = tabs.find((tab) =>
+      tab.content.some((content) => content.items.some((i) => i.id === item.id))
+    )
+
+    const currentSection = currentTab.content.find((content) =>
+      content.items.some((i) => item.id === i.id)
+    )
+
+    const destinySection = currentTab.content.find((content) =>
+      content.items.every((i) => item.id !== i.id)
+    )
+
+    if (currentSection) {
+      currentSection.items = currentSection.items.filter(
+        (i) => i.id !== item.id
+      )
+    }
+
+    if (destinySection) {
+      const newItem = { ...item, completed: !item.completed }
+      destinySection.items = [...destinySection.items, newItem]
+    }
+
+    set((state) => ({
+      tabs: state.tabs
+    }))
+  }
+}))
